89960bbfe8c469ed593dec993e91d5ec8e1adec6
ankit2021te <ankit2021te@gmail.com>
2026-02-01 15:43:45 +0530

    alert and frameHandling Code

---
+++ src/test/java/org/launch/program/basic/FrameHandling.java
@@ -0,0 +1,9 @@
+package org.launch.program.basic;
+
+public class FrameHandling {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+    }
+}
